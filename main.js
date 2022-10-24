12835cb9da75f527c3fde9e7321e79dd0114f127
wmckeever <wil.mckeever@gmail.com>
2022-10-24 15:36:57 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+
+let p1 = document.querySelector('p');
+p1.style.color="lightblue";
+
+p1.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+
+let header = document.querySelector('h1');
+header.style.fontSize = "48px";
+
+let item3 = document.querySelector('#item-3')
+item3.innerText = "I say, Hi!";
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = "0.5";
+
+let myImage = document.querySelector('img');
+
+myImage.src ="https://www.thecoderpedia.com/wp-content/uploads/2020/06/There-are-two-ways-to-write-error-free-programs-Technology-Quotes.png?x34900";
+
+myImage.style.height = "300px"
+
+let myImage2 = document.querySelector('#img-2');
+
+myImage2.src ="https://quotefancy.com/media/wallpaper/3840x2160/2000881-Joel-Spolsky-Quote-It-s-harder-to-read-code-than-to-write-it.jpg";
+
+myImage2.style.height ="300px";
+
+let item16 = document.querySelector('#item-16');
+item16.innerText = "Won't get fooled again.";
